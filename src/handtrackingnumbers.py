17e9b2f87513f12d4b81987ca439bee26e35608b
Dol0resH8ze <yelaissaoui80@gmail.com>
2025-11-16 05:23:57 +0100

    Refactor hand tracking to support handedness detection

--- src/handtrackingnumbers.py
+++ src/handtrackingnumbers.py
@@ -2,26 +2,22 @@ import cv2
 import mediapipe as mp
 import time
 
-def fingers_up(hand_landmarks):
+def fingers_up(hand_landmarks, handedness="Right"):
     fingers = []
 
     # Thumb
-    if hand_landmarks.landmark[4].x < hand_landmarks.landmark[3].x:
-        fingers.append(1)
-    else:
-        fingers.append(0)
+    if handedness == "Right":
+        fingers.append(1 if hand_landmarks.landmark[4].x < hand_landmarks.landmark[3].x else 0)
+    else:  # Left hand
+        fingers.append(1 if hand_landmarks.landmark[4].x > hand_landmarks.landmark[3].x else 0)
 
     # Other 4 fingers
     for tip_id, pip_id in zip([8, 12, 16, 20], [6, 10, 14, 18]):
-        if hand_landmarks.landmark[tip_id].y < hand_landmarks.landmark[pip_id].y:
-            fingers.append(1)
-        else:
-            fingers.append(0)
+        fingers.append(1 if hand_landmarks.landmark[tip_id].y < hand_landmarks.landmark[pip_id].y else 0)
 
     return fingers  
 
 def detect_numbers(fingers):
-    
     if fingers == [0, 0, 0, 0, 0]:
         return "0"
     elif fingers == [0, 1, 0, 0, 0]:
@@ -41,11 +37,10 @@ def main():
     cap = cv2.VideoCapture(0)
 
     mpHands = mp.solutions.hands
-    hands = mpHands.Hands()
+    hands = mpHands.Hands(max_num_hands=2, min_detection_confidence=0.7, min_tracking_confidence=0.7)
     mpDraw = mp.solutions.drawing_utils
 
     pTime = 0
-    cTime = 0
 
     while True:
         success, img = cap.read()
@@ -55,30 +50,26 @@ def main():
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         results = hands.process(imgRGB)
 
-        number = ""
-
-        if results.multi_hand_landmarks:
-            for handLms in results.multi_hand_landmarks:
-                
-                
-                fingers = fingers_up(handLms)
-                number = detect_numbers(fingers)
-                
-                mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
-
         cTime = time.time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
 
-        # Show FPS
+        if results.multi_hand_landmarks:
+            for idx, handLms in enumerate(results.multi_hand_landmarks):
+                handedness_label = results.multi_handedness[idx].classification[0].label
+                fingers = fingers_up(handLms, handedness_label)
+                number = detect_numbers(fingers)
+
+                mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+                h, w, _ = img.shape
+                cx, cy = int(handLms.landmark[0].x * w), int(handLms.landmark[0].y * h)
+                if number != "":
+                    cv2.putText(img, f'{number}', (500, 150 ), cv2.FONT_HERSHEY_SIMPLEX, 5, (255, 255, 255), 3)
+
         cv2.putText(img, f'FPS: {int(fps)}', (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 3)
-        # Show detected number
-        if number != "":
-            cv2.putText(img, f'{number}', (500, 150), cv2.FONT_HERSHEY_SIMPLEX, 4, (255, 255, 255), 5)
+        cv2.imshow("Hand Tracking Numbers", img)
 
-        cv2.imshow("Hand Tracking", img)
-
-        # Exit on pressing 'q' 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
